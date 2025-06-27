492af7ba49cd9bb404b08db95d735e18932511b4
Lg <gabriel.2020.com@gmail.com>
2025-06-27 19:22:33 -0300

    Exception handling - delete

--- src/main/java/com/github/luisgabrieldev/ecommerce_api/services/UserService.java
+++ src/main/java/com/github/luisgabrieldev/ecommerce_api/services/UserService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.github.luisgabrieldev.ecommerce_api.entities.User;
 import com.github.luisgabrieldev.ecommerce_api.repositories.UserRepository;
+import com.github.luisgabrieldev.ecommerce_api.services.exceptions.DatabaseException;
 import com.github.luisgabrieldev.ecommerce_api.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,8 +32,14 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
-	}
+	    repository.findById(id).orElseThrow(
+	            () -> new ResourceNotFoundException(id)); 
+	        try { 
+	            repository.deleteById(id); 
+	        } catch (DataIntegrityViolationException e) { 
+	            throw new DatabaseException(e.getMessage()); 
+	        }  
+	    }
 	
 	public User update(Long id, User obj) {
 		User entity = repository.getReferenceById(id);
