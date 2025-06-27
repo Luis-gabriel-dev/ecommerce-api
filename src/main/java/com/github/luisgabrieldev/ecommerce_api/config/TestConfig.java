56ef346a82a99c7d1012a6d716028ecfa5ed0fb6
Lg <gabriel.2020.com@gmail.com>
2025-06-27 13:12:56 -0300

    Payment, one-to-one association

--- src/main/java/com/github/luisgabrieldev/ecommerce_api/config/TestConfig.java
+++ src/main/java/com/github/luisgabrieldev/ecommerce_api/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.github.luisgabrieldev.ecommerce_api.entities.Category;
 import com.github.luisgabrieldev.ecommerce_api.entities.Order;
 import com.github.luisgabrieldev.ecommerce_api.entities.OrderItem;
+import com.github.luisgabrieldev.ecommerce_api.entities.Payment;
 import com.github.luisgabrieldev.ecommerce_api.entities.Product;
 import com.github.luisgabrieldev.ecommerce_api.entities.User;
 import com.github.luisgabrieldev.ecommerce_api.entities.enums.OrderStatus;
@@ -80,5 +81,10 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null,Instant.parse("2025-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
